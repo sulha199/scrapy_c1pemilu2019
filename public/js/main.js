600a75c802c0856aa9995617e64e998e396f081f
root <root@SIK-SHULHA.localdomain>
2019-05-04 22:54:28 +0700

    update filelist url

--- public/js/main.js
+++ public/js/main.js
@@ -54,19 +54,28 @@ var gridOptions = {
 file_list = [];
 
 function getFileList() {
-	$.get("https://raw.githubusercontent.com/sulha199/scrapy_c1pemilu2019/master/public/data/file_list.txt", data => {
-		file_list = data.split("\n");
-		latestKomparasi = getKomparasiList(file_list);
-		fileParam = getUrlVars();
-		loadOldVersionList(latestKomparasi);
-		loadKomparasiData(fileParam['file'] || latestKomparasi[0]);
-	});
+  $.get(
+    "https://raw.githubusercontent.com/sulha199/scrapy_c1pemilu2019/master/public/data/file_list.txt",
+    data => {
+      file_list = data.split("\n");
+      latestKomparasi = getKomparasiList(file_list);
+      fileParam = getUrlVars();
+      loadOldVersionList(latestKomparasi);
+      loadKomparasiData(fileParam["file"] || latestKomparasi[0]);
+    }
+  );
 }
 
 function loadKomparasiData(filename) {
   agGrid.simpleHttpRequest({ url: "data/" + filename }).then(function(data) {
-		gridOptions.api.setRowData(data);
-		$('#data-time').html(filename.replace('komparasi_','').replace('.json','').replace('_', ' ').replace('.',':'))
+    gridOptions.api.setRowData(data);
+    $("#data-time").html(
+      filename
+        .replace("komparasi_", "")
+        .replace(".json", "")
+        .replace("_", " ")
+        .replace(".", ":")
+    );
   });
   // setup the grid after the page has finished loading
   document.addEventListener("DOMContentLoaded", function() {
@@ -77,45 +86,41 @@ function loadKomparasiData(filename) {
 }
 
 function getKomparasiList(list) {
-  return list.filter(d =>
-    d
-      .includes("komparasi_")
-      .sort()
-      .reverse()
-  );
+  return list
+    .filter(d => d.includes("komparasi_"))
+    .sort()
+    .reverse();
 }
 
 function getKpuDataList(list) {
-  return list.filter(d =>
-    d
-      .includes("kpu_")
-      .sort()
-      .reverse()
-  );
+  return list
+    .filter(d => d.includes("kpu_"))
+    .sort()
+    .reverse();
 }
 
 function getKawalDataList(list) {
-  return list.filter(d =>
-    d
-      .includes("kawal_")
-      .sort()
-      .reverse()
-  );
+  return list
+    .filter(d => d.includes("kawal_"))
+    .sort()
+    .reverse();
 }
 
 function loadOldVersionList(list) {
-	list.map(filename => {
-		$('#komparasi-list').append('<li><a href="?file=' + filename +'">' + filename +'</a></li>')
-	});
+  list.map(filename => {
+    $("#komparasi-list").append(
+      '<li><a href="?file=' + filename + '">' + filename + "</a></li>"
+    );
+  });
 }
 
 function openVersions() {
-	$( "#old-version-selector" ).dialog();
+  $("#old-version-selector").dialog();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
   var gridDiv = document.querySelector("#myGrid");
-	new agGrid.Grid(gridDiv, gridOptions);
-	
-	getFileList();
+  new agGrid.Grid(gridDiv, gridOptions);
+
+  getFileList();
 });
