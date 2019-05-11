89429c66e07015ab233b92f02c0ddac87f6e85f2
shulha yahya <sulha199@gmail.com>
2019-05-12 02:21:58 +0700

    update UI and remove csv file

--- public/js/vars.js
+++ public/js/vars.js
@@ -4,25 +4,25 @@ var columnDefs = [
   {
     headerName: "Propinsi",
     field: "propinsi",
-    width: 150,
+    width: 140,
     sortable: true
   },
   {
     headerName: "Kab/Kota",
     field: "kab",
-    width: 150,
+    width: 140,
     sortable: true
   },
   {
     headerName: "Kecamatan",
     field: "kec",
-    width: 150,
+    width: 140,
     sortable: true
   },
   {
     headerName: "Kelurahan",
     field: "kel",
-    width: 150,
+    width: 140,
     sortable: true
   },
   {
@@ -34,35 +34,79 @@ var columnDefs = [
   {
     headerName: "pemilu2019.kpu.go.id/",
     children: [
-      { headerName: "01", field: "p1", width: 70, sortable: true },
-      { headerName: "02", field: "p2", width: 70, sortable: true },
+      {
+        headerName: "01",
+        field: "p1",
+        width: 70,
+        sortable: true,
+        cellStyle: { "background-color": "#bde2e5" }
+      },
+      {
+        headerName: "02",
+        field: "p2",
+        width: 70,
+        sortable: true,
+        cellStyle: { "background-color": "#cec" }
+      },
       {
         headerName: "Suara Valid",
         field: "suara_valid",
         width: 70,
-        sortable: true
+        sortable: true,
+        cellStyle: { "background-color": "#ececec" },
       },
-      { headerName: "DPT awal", field: "dpt", width: 70, sortable: true },
+      {
+        headerName: "DPT awal",
+        field: "dpt",
+        width: 70,
+        sortable: true,
+        cellStyle: { "background-color": "#ececec" },
+      },
       {
         headerName: "Presentase Suara Valid / DPT (%)",
         field: "presentase",
-        width: 130,
-        sortable: true
+        width: 110,
+        sortable: true,
+        cellStyle: { "background-color": "#ececec" },
       }
     ]
   },
   {
     headerName: "pantau.kawalpilpres2019.id",
     children: [
-      { headerName: "01", field: "kawal_p1", width: 70, sortable: true },
-      { headerName: "02", field: "kawal_p2", width: 70, sortable: true }
+      {
+        headerName: "01",
+        field: "kawal_p1",
+        width: 70,
+        sortable: true,
+        cellStyle: { "background-color": "#bde2e5" }
+      },
+      {
+        headerName: "02",
+        field: "kawal_p2",
+        width: 70,
+        sortable: true,
+        cellStyle: { "background-color": "#cec" }
+      }
     ]
   },
   {
     headerName: "ayojagatps.com",
     children: [
-      { headerName: "01", field: "ayo_p1", width: 70, sortable: true },
-      { headerName: "02", field: "ayo_p2", width: 70, sortable: true }
+      {
+        headerName: "01",
+        field: "ayo_p1",
+        width: 70,
+        sortable: true,
+        cellStyle: { "background-color": "#bde2e5" }        
+      },
+      {
+        headerName: "02",
+        field: "ayo_p2",
+        width: 70,
+        sortable: true,
+        cellStyle: { "background-color": "#cec" }
+      }
     ]
   }
 ];
@@ -80,7 +124,7 @@ var gridOptions = {
   headerHeight: headerHeight
 };
 
-var topGridOptions = {
+var notGridOptions = {
   defaultColDef: {
     sortable: true,
     resizable: true,
